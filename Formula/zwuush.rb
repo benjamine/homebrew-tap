f5b6b385c7fc5245feef28d53e5f53470a418fd8
Benjamín Eidelman <beneidel+gh@gmail.com>
2024-09-06 17:05:35 -0300

    zwuush@0.0.2

--- Formula/zwuush.rb
+++ Formula/zwuush.rb
@@ -1,8 +1,8 @@
 class Zwuush < Formula
     desc ""
     homepage "https://github.com/benjamine/zwuush"
-    url "https://github.com/benjamine/zwuush/releases/download/v0.0.1/zwuush"
-    sha256 "e67a3d32a7528366b6cb55b038909d03e50203145fab637831cdd4ac960d4eaa"
+    url "https://github.com/benjamine/zwuush/releases/download/v0.0.2/zwuush"
+    sha256 "8ceb88804b27336e355cef2ab3a02cf3b39be1fd03da3e96d44f951b614b6bee"
     license ""
   
     def install
@@ -10,7 +10,7 @@ class Zwuush < Formula
     end
   
     test do
-      system "#{bin}/yourapp", "--version"
+      system "#{bin}/zwuush", "--version"
     end
   end
   
